90907a6a9e0549f5552e07bb63ef4442297b1c0c
Oooleaf <114543429+Oooleaf@users.noreply.github.com>
2022-11-20 08:24:28 -0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Lab 8 - Starter
+
+
+## Check Your Understanding
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+-Within a Github action that runs whenever code is pushed 
+-Manually run them locally before pushing code
+-Run them all after all development is completed
+
+Select: -Within a Github action that runs whenever code is pushed 
+
+Because we want to check whether our code run as our expection, run and pass all the automated tests, make this simple and efficient.
+
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+   
+   No. Because it an end to end test does not check if a function is returning the correct output. Rather than that, it only check whether all functions can run from start to end. We prefer to use unit test here.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   
+   No. Because "message" feature is not just one function that can be test with unit test.
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+    Yes. Beccause we can use one function to test the "max message length" feature.
+
+## Screenshots
+![1](s1.png)
+![2](s2.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,95 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//test phone numbers
+test('Phone number returns true #1 ', () => {
+    
+    expect(functions.isPhoneNumber('626-666-6666')).toBe(true);
+});
+test('Phone number returns true #2', () => {
+    
+    expect(functions.isPhoneNumber('(666)666-6666')).toBe(true);
+});
+test('Phone number returns false #1', () => {
+    
+    expect(functions.isPhoneNumber('666')).toBe(false);
+});
+test('Phone number returns false #2 ', () => {
+    
+    expect(functions.isPhoneNumber('6')).toBe(false);
+});
+
+//test emails
+test('Email true #1', () => {
+    
+    expect(functions.isEmail('666@gmail.com')).toBe(true);
+});
+test('Email true #2', () => {
+    
+    expect(functions.isEmail('666@ucsd.edu')).toBe(true);
+});
+
+test('Email false #1', () => {
+    
+    expect(functions.isEmail('666@qq')).toBe(false);
+});
+
+test('Email false #2', () => {
+    
+    expect(functions.isEmail('666@***.com')).toBe(false);
+});
+
+//test dates
+test('dates true #1', () => {
+    
+    expect(functions.isDate('11/20/2022')).toBe(true);
+});
+test('dates true #2', () => {
+    
+    expect(functions.isDate('11/20/2020')).toBe(true);
+});
+
+test('dates false #1', () => {
+    
+    expect(functions.isDate('2022/2022/2022')).toBe(false);
+});
+test('dates false #2', () => {
+    
+    expect(functions.isDate('666-666-666')).toBe(false);
+});
+
+//test hex color
+test('hex true #1', () => {
+    
+    expect(functions.isHexColor('#EEEEEE')).toBe(true);
+});
+test('hex true #2', () => {
+    
+    expect(functions.isHexColor('#32a852')).toBe(true);
+});
+test('hex false #1', () => {
+    
+    expect(functions.isHexColor('##32a852')).toBe(false);
+});
+test('hex false #2', () => {
+    
+    expect(functions.isHexColor('32a85qqq')).toBe(false);
+});
+
+//test strong password
+test('strong password true #1', () => {
+    
+    expect(functions.isStrongPassword('ddddddd')).toBe(true);
+});
+test('strong password true #2', () => {
+    
+    expect(functions.isStrongPassword('QqqQqqQ')).toBe(true);
+});
+test('strong password false #1', () => {
+    
+    expect(functions.isStrongPassword('666')).toBe(false);
+});
+test('strong password false #2', () => {
+    
+    expect(functions.isStrongPassword('qwq')).toBe(false);
+});
